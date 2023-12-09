9e75c498b9260ec77f9c2f54c97d2e089fa16289
ZhengYinan-AIR <83967999+ZhengYinan-AIR@users.noreply.github.com>
2023-12-10 00:03:47 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,14 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=False
 export CUDA_VISIBLE_DEVICES=0
 # export JAX_PLATFORM_NAME=cpu
 
-# python launcher/examples/train_offline.py --env_id 0 --config configs/train_config.py:fisor_cb1
 
-# python launcher/examples/train_offline.py --env_id 0 --config configs/train_config.py:fisor_cb1
-# python launcher/examples/train_offline.py --env_id 21 --config configs/train_config.py:fisor --ratio 0.1
-# python launcher/examples/train_offline.py --env_id 21 --config configs/train_config.py:fisor --ratio 0.1
-# 
+# main result
+python launcher/examples/train_offline.py --env_id 0 --config configs/train_config.py:fisor
 
-python launcher/examples/train_offline.py --env_id 29 --config configs/train_config.py:fisor
+
+# data quantity
+# python launcher/examples/train_offline.py --env_id 17 --config configs/train_config.py:fisor --ratio 0.1
+# python launcher/examples/train_offline.py --env_id 17 --config configs/train_config.py:fisor --ratio 0.5
+
+# python launcher/examples/train_offline.py --env_id 21 --config configs/train_config.py:fisor --ratio 0.1
+# python launcher/examples/train_offline.py --env_id 21 --config configs/train_config.py:fisor --ratio 0.5
